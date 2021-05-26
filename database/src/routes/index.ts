74eff03200f375be1f90d0b1fcf39a39f3e68eda
Erwin Kok <erwinkok@erwins-mbp.kpn>
2021-05-26 19:45:44 +0200

    Remove all the things

--- database/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import ProductRoutes from "./product.route";
-import {Application} from "express";
-
-const configureRoutes = (app: Application) => {
-    app.use('/api/product/', ProductRoutes);
-};
-
-export default configureRoutes;
